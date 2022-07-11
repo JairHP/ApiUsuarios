b1c311d4aca7f99cc6dd99378850b88b759d29da
Adriana Mendoza <20193l001018@utcv.edu.mx>
2022-07-10 23:30:06 -0500

    Se agregaron las rutas correspondientes al crud de clientes en el archivo api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\clienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('clientes', [clienteController::class, 'list']);
+Route::get('clientes/{cliente}', [clienteController::class, 'show']);
+Route::post('clientes', [clienteController::class, 'store']);
+Route::put('clientes/{cliente}', [clienteController::class, 'update']);
+Route::delete('clientes/{cliente}', [clienteController::class, 'destroy']);
